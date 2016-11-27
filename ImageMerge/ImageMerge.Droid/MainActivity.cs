d58b7cfb347862512781112bd239b835596fcfd8
Maciej Chmielewski <maciej.chmielewski@powel.com>
2016-11-27 16:17:01 +0100

    app name

--- ImageMerge/ImageMerge.Droid/MainActivity.cs
+++ ImageMerge/ImageMerge.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace ImageMerge.Droid
 {
-    [Activity(Label = "ImageMerge", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Auto Meme", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
